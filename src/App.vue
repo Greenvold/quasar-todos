b7239d5206b1e91f4169b533689fb4a847316abf
Rudolf <rudolf.bruder@gmail.com>
2020-03-25 14:32:23 +0100

    Editing and style todos on main page

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
+<style>
+.text-strikethrough {
+  text-decoration: line-through;
+}
+</style>
--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,11 +1,73 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+  <q-page class="q-pa-md">
+    <q-list separator bordered>
+      <q-item-label header>General</q-item-label>
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        v-ripple
+        clickable
+        @click="task.completed =!task.completed"
+        :class="task.completed ? 'bg-orange-1' : ''"
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label :class="{'text-strikethrough' : task.completed}">{{task.name}}</q-item-label>
+          <q-item-label caption>{{task.desc}}</q-item-label>
+        </q-item-section>
+        <q-item-section side>
+          <div class="row">
+            <div class="column justify-center">
+              <q-icon name="event" size="18px" class="q-mr-sm" />
+            </div>
+            <div class="column">
+              <q-item-label class="row justify-end" caption>{{task.dueDate}}</q-item-label>
+              <q-item-label class="row justify-end" caption>
+                <small>{{task.dueTime}}</small>
+              </q-item-label>
+            </div>
+          </div>
+        </q-item-section>
+      </q-item>
+      <q-item v-ripple></q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: "PageIndex"
+  name: "PageIndex",
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: "Go to Shop",
+          desc: "Buy all the stuff you can find",
+          dueDate: "2019/05/12",
+          dueTime: "18:30",
+          completed: false
+        },
+        {
+          id: 2,
+          name: "Get the XRP",
+          desc: "Check the crypto market daily for prices changes.",
+          dueDate: "2019/05/13",
+          dueTime: "14:30",
+          completed: false
+        },
+        {
+          id: 3,
+          name: "Watch the stocks",
+          desc: "What happens on the stock. Check it on daily routine.",
+          dueDate: "2019/05/11",
+          dueTime: "15:30",
+          completed: true
+        }
+      ]
+    };
+  }
 };
 </script>
